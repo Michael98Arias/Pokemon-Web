4eb5636c28ce487dc97c801ab1c7aca60c504f78
Michael98Arias <maicol98arias@gmail.com>
2024-06-12 20:49:40 -0500

    Configure and consume api

--- src/components/MainLayout/List/ListApi.ts
+++ src/components/MainLayout/List/ListApi.ts
@@ -1,8 +1,16 @@
-// import { } from './interfaces/List';
+import HttpService from '../../../shared/services/HttpService';
 
+import {ListInitial, DataItem } from './interfaces/List';
 
 class ListApi {
-
+    public ListInitialApi = (): Promise<ListInitial> => {
+		const url = 'pokemon'
+		return HttpService.get(url)
+	};
+	public DataItemApi = (name:string): Promise<DataItem> => {
+		const url = `pokemon/${name}`
+		return HttpService.get(url)
+	};
 }
 
 export default new ListApi();
